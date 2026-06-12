814b3bad35ebfcefaeac046ee77ef736e71c1ca7
rareedge1 <randyranoyr@gmail.com>
2026-06-12 16:44:46 -0500

    re-calibration.js

--- public/re-calibration.js
+++ public/re-calibration.js
@@ -1,13 +1,7 @@
 function ROICalculator({ wins, losses, pushes }) {
-  return <div style={{color:"white",padding:"20px"}}>ROI: {wins}W {losses}L</div>;
+  return React.createElement("div", {style:{color:"white",padding:"20px"}}, "ROI: " + wins + "W " + losses + "L");
 }
 
 function CalibrationDashboard({ isPremium }) {
-  return (
-    <div style={{color:"white",padding:"20px"}}>
-      <div>Calibration Tab Loading...</div>
-      <div>Premium: {isPremium ? "yes" : "no"}</div>
-      <ROICalculator wins={5} losses={2} pushes={0} />
-    </div>
-  );
+  return React.createElement("div", {style:{color:"white",padding:"20px"}}, "Calibration works! Premium: " + isPremium);
 }
